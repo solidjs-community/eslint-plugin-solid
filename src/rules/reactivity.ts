7c041b8e0c16b9df955da3aa2f7079851da8bdc2
Josh Wilson <joshwilsonvu@gmail.com>
2021-12-21 08:51:25 -0500

    Working on reactivity rule.

--- src/rules/reactivity.ts
+++ src/rules/reactivity.ts
@@ -25,26 +25,157 @@
  * functions containing one of these to the list, and grab their scopes with eslint-utils.getInnermostScope(). Using a
  * WeakMap for caching, we'll calculate if the scope is a tracking scope, and warn if a reactive value is used in a
  * non-tracked scope.
+ *
+ * Find all tracked scopes in a function, and then traverse the enclosing function, skipping those scopes,
+ * looking for reactive expressions and reporting them.
+ *
+ * Consider the following code:
+ * ```
+ * function Component() {
+ *   const [signal, setSignal] = createSignal();
+ *   const innerFn = () => signal(); // ok
+ *   signal(); // not ok
+ *   innerFn(); // not ok
+ *   return <div>{signal()}{innerFn()}</div>; // ok
+ * }
+ * ```
+ * It's not okay to access signals in the same scope they're declared in, but it's okay to
+ * use them one or more nested functions down. However, that makes the nested functions act
+ * like signals.
  */
 
-/* eslint-disable */
-
-import type { Rule } from "eslint";
+import type { Rule, Scope } from "eslint";
+import { findVariable, getInnermostScope } from "eslint-utils";
 import esquery from "esquery";
+import {
+  ArrowFunctionExpression,
+  CallExpression,
+  Expression,
+  FunctionDeclaration,
+  FunctionExpression,
+  Identifier,
+  JSXExpressionContainer,
+  Node,
+  Program,
+  ReturnStatement,
+  VariableDeclaration,
+  VariableDeclarator,
+} from "estree-jsx";
+import { isReturningJSX } from "../utils/ast";
+
+const isCalledWithFunction = (node: CallExpression, arg: number): boolean => {
+  return node.arguments.length > arg && node.arguments[arg].type.includes("FunctionExpression");
+};
 
 const rule: Rule.RuleModule = {
   meta: {
     type: "problem",
     docs: {
-      description: "Ensures that component props are evaluated lazily, not destructured, ",
+      description:
+        "Enforce that reactive expressions (props, signals, memos, etc.) are only used in tracked scopes.",
+      url: "https://github.com/joshwilsonvu/eslint-plugin-solid/blob/main/docs/reactivity.md",
     },
-    messages: {},
     fixable: "code",
+    schema: [],
+    messages: {},
   },
   create(context): Rule.RuleListener {
     // a rule is created for every source file, so it's reasonable to create a cache here
     // const isReactiveCache: Set<Rule.Node> = new Set();
-    return {};
+
+    const sourceCode = context.getSourceCode();
+    const isProps = (node: Identifier) => {
+      const variable = findVariable(context.getScope(), node);
+      if (variable) {
+        console.log("DEFINITIONS", variable.defs);
+        const def = variable.defs[variable.defs.length - 1];
+        if (
+          def &&
+          def.type === "Parameter" &&
+          def.node.params.length === 1 &&
+          isReturningJSX(def.node)
+        ) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+
+    const trackedScopes = new Set<Node>();
+    const components = new Set<
+      FunctionExpression | ArrowFunctionExpression | FunctionDeclaration
+    >();
+    const signalLikes = new Set<Scope.Variable | CallExpression>();
+    const propsLikes = new Set<Scope.Variable | CallExpression>();
+
+    return {
+      /* Tracking scopes: */
+      JSXExpressionContainer(node: Node) {
+        trackedScopes.add(node);
+      },
+      CallExpression(node: CallExpression) {
+        // createEffect, createMemo fn arg
+        if (node.callee.type !== "Identifier") {
+          return; // nothing to do for member expression calls
+        }
+        if (
+          ["createEffect", "createMemo"].includes(node.callee.name) &&
+          isCalledWithFunction(node, 0)
+        ) {
+          trackedScopes.add(node.arguments[0]);
+        } else if (
+          node.callee.name === "createResource" &&
+          node.arguments.length > 0 &&
+          !node.arguments[0].type.includes("FunctionExpression")
+        ) {
+          // createResource optional `source` first argument may be a signal
+          trackedScopes.add(node.arguments[0]);
+        }
+      },
+
+      /* Reactive expressions: */
+      VariableDeclarator(node: VariableDeclarator) {
+        if (!node.init) {
+          return;
+        }
+        const getFirstDestructuredVar = (id: Node): Scope.Variable | null => {
+          if (id.type === "ArrayPattern" && id.elements[0]?.type === "Identifier") {
+            return findVariable(context.getScope(), id.elements[0].name);
+          }
+          return null;
+        };
+        const getReturnedVar = (id: Node): Scope.Variable | null => {
+          if (id.type === "Identifier") {
+            return findVariable(context.getScope(), id.name);
+          }
+          return null;
+        };
+
+        // Mark return values of certain functions as reactive
+        if (node.init.type === "CallExpression" && node.init.callee.type === "Identifier") {
+          const { callee } = node.init;
+          if (callee.name === "createSignal" || callee.name === "useTransition") {
+            const signal = getFirstDestructuredVar(node.id);
+            if (signal) {
+              signalLikes.add(signal);
+            }
+          } else if (callee.name === "createMemo") {
+            const memo = getReturnedVar(node.id);
+            if (memo) {
+              signalLikes.add(memo); // memos act like signals
+            } else {
+              signalLikes.add(node.init); // can be tracked as a node instead of an expression
+            }
+          } else if (callee.name === "createStore") {
+            const store = getFirstDestructuredVar(node.id);
+            if (store) {
+              propsLikes.add(store); // stores act like props
+            }
+          }
+        }
+      },
+    };
   },
 };
 
