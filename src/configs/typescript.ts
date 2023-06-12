61380f7703e57d2570b67103b1dceacd84776300
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-06-12 13:38:50 +0200

    Use recommended rules as base for typescript rules
    This should make it easier to maintain the configs as changes to the recommended config won't need to be copypasted over to the TS config and it's more immediatly clear where the configs differ.

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -1,4 +1,5 @@
 import { plugin } from "../plugin";
+import recommended from "./recommended";
 
 const typescript = {
   files: ["**/*.js?(x)", "**/*.ts?(x)"],
@@ -14,36 +15,11 @@ const typescript = {
       },
     },
   },
-  rules: {
-    // identifier usage is important
-    "solid/jsx-no-duplicate-props": 2,
+  rules: Object.assign({}, recommended.rules, {
     "solid/jsx-no-undef": [2, { typescriptEnabled: true }],
-    "solid/jsx-uses-vars": 2,
-    // security problems
-    "solid/no-innerhtml": 2,
-    "solid/jsx-no-script-url": 2,
-    // reactivity
-    "solid/components-return-once": 1,
-    "solid/no-destructure": 2,
-    "solid/prefer-for": 2,
-    "solid/reactivity": 1,
-    "solid/event-handlers": 1,
-    // these rules are mostly style suggestions
-    "solid/imports": 1,
-    "solid/style-prop": 1,
-    "solid/no-react-deps": 1,
-    "solid/no-react-specific-props": 1,
-    "solid/self-closing-comp": 1,
-    "solid/no-array-handlers": 0,
     // namespaces taken care of by TS
     "solid/no-unknown-namespaces": 0,
-    // handled by Solid compiler, opt-in style suggestion
-    "solid/prefer-show": 0,
-    // only necessary for resource-constrained environments
-    "solid/no-proxy-apis": 0,
-    // deprecated
-    "solid/prefer-classlist": 0,
-  },
+  }),
 };
 
 export = typescript;
