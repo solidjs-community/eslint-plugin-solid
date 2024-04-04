900826b495ac4375e0f9a08db6d0ed86834d9eb6
Han Yeong-woo <han@yeongwoo.dev>
2024-04-04 16:48:28 +0900

    Deprecate flat config entry points

--- configs/typescript.d.ts
+++ configs/typescript.d.ts
@@ -1,4 +1,9 @@
 declare module "eslint-plugin-solid/configs/typescript" {
+  /**
+   * @deprecated Please use flat configs via the root entry ("eslint-plugin-solid").
+   * Refer to the [README](https://github.com/solidjs-community/eslint-plugin-solid#flat-configuration)
+   * for more details.
+   */
   const config: typeof import("../dist/index").configs["flat/typescript"];
   export = config;
 }
