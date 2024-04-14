24ea3a649012ec0ba6eb55a90e4c9cae05d7dc44
Josh Wilson <joshwilsonvu@gmail.com>
2024-04-14 16:51:04 -0400

    WIP starting compat file for a bunch of stupid API changes with no backwards-compatibility.

---
+++ src/compat.ts
@@ -0,0 +1,6 @@
+// import { TSESLint } from "@typescript-eslint/utils";
+// import type { ESLint } from 'eslint-v8';
+
+// // export default function compat(context: TSESLint.RuleContext<never, readonly Array<unknown>>) {
+// //   const sourceCode = typeof
+// // }
--- test/ruleTester.ts
+++ test/ruleTester.ts
@@ -1,17 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { TSESLint } from "@typescript-eslint/utils";
+import { RuleTester } from "eslint";
 // @ts-ignore
 import { RuleTester as RuleTester_v6 } from "eslint-v6";
 // @ts-ignore
 import { RuleTester as RuleTester_v7 } from "eslint-v7";
-
-const RuleTester = TSESLint.RuleTester;
+// @ts-ignore
+import { RuleTester as RuleTester_v8 } from "eslint-v8";
+import type { TSESLint } from "@typescript-eslint/utils";
 
 // The default parser
 const jsxTester = new RuleTester({
-  // @ts-ignore
-  parser: undefined,
-  parserOptions: {
+  languageOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
@@ -21,7 +20,7 @@ const jsxTester = new RuleTester({
 });
 
 // spread ...tsOnlyTest into a test case to enforce it always runs with the TS parser
-export const tsOnlyTest: Pick<TSESLint.RuleTesterConfig, "parser" | "parserOptions"> = {
+export const tsOnlyTest = {
   parser: require.resolve("@typescript-eslint/parser"),
   parserOptions: {
     ecmaVersion: 2018,
@@ -50,16 +49,6 @@ const babelTester = new RuleTester({
   },
 });
 
-const v7Tester = new RuleTester_v7({
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-});
-
 const v6Tester = new RuleTester_v6({
   parserOptions: {
     ecmaVersion: 2018,
@@ -70,7 +59,41 @@ const v6Tester = new RuleTester_v6({
   },
 });
 
-export const run: typeof TSESLint.RuleTester.prototype.run = (name, rule, tests) => {
+const v7Tester = new RuleTester_v7({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+
+const v8Tester = new RuleTester_v8({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+
+interface Tests {
+  valid?: Array<(RuleTester.ValidTestCase & { tsOnly?: boolean }) | string>;
+  invalid?: Array<RuleTester.InvalidTestCase & { tsOnly?: boolean }>;
+}
+export const run = (
+  name: string,
+  rule: TSESLint.RuleModule<never, Array<unknown>>,
+  tests: Tests
+) => {
+  const jsOnlyPredicate = (test: { tsOnly?: boolean } | string) =>
+    !(typeof test === "object" && test.tsOnly);
+  const jsTests = {
+    valid: tests.valid?.filter(jsOnlyPredicate) as Array<RuleTester.ValidTestCase | string>,
+    invalid: tests.invalid?.filter(jsOnlyPredicate) as Array<RuleTester.InvalidTestCase>,
+  };
   switch (process.env.PARSER) {
     case "ts":
       describe("@typescript-eslint/parser", () => tsTester.run(name, rule, tests));
@@ -78,23 +101,27 @@ export const run: typeof TSESLint.RuleTester.prototype.run = (name, rule, tests)
     case "babel":
       describe("@babel/eslint-parser", () => babelTester.run(name, rule, tests));
       break;
-    case "v7":
-      describe("eslint v7", () => v7Tester.run(name, rule, tests));
-      break;
     case "v6":
-      describe("eslint v6", () => v6Tester.run(name, rule, tests));
+      describe("eslint v6", () => v6Tester.run(name, rule, jsTests));
+      break;
+    case "v7":
+      describe("eslint v7", () => v7Tester.run(name, rule, jsTests));
+      break;
+    case "v8":
+      describe("eslint v8", () => v8Tester.run(name, rule, jsTests));
       break;
     case "all":
-      describe("esprima", () => jsxTester.run(name, rule, tests));
+      describe("esprima", () => jsxTester.run(name, rule, jsTests));
       describe("@typescript-eslint/parser", () => tsTester.run(name, rule, tests));
       describe("@babel/eslint-parser", () => babelTester.run(name, rule, tests));
-      describe("eslint v7", () => v7Tester.run(name, rule, tests));
-      describe("eslint v6", () => v6Tester.run(name, rule, tests));
+      describe("eslint v6", () => v6Tester.run(name, rule, jsTests));
+      describe("eslint v7", () => v7Tester.run(name, rule, jsTests));
+      describe("eslint v8", () => v8Tester.run(name, rule, jsTests));
       break;
     case "none":
       break;
     default:
-      describe("esprima", () => jsxTester.run(name, rule, tests));
+      describe("esprima", () => jsxTester.run(name, rule, jsTests));
       break;
   }
   return tests;
